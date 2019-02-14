eeae225b51f233b02178c58614f03dc663ed95bd
gabriela <gabriela@WS30007>
2019-02-14 17:10:57 +0200

    genurile

---
+++ student/DbCopchii.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Text;
+
+namespace student
+{
+    public class DbCopchii
+    {
+        public List<Elev> GetCophii()
+        {
+           
+            Elev elev = new Elev();
+            elev.Nume = "Grey";
+            elev.Prenume = "Christian";
+            elev.IDNP = "2012012004459";
+            elev.Clasa = 8;
+            elev.DataNaserii = new DateTime(1998, 8, 11);
+            elev.Genul = Gen.Masculin;
+            elev.Adresa = new Address();
+
+            elev.Adresa.Strada = "Central Park";
+            elev.Adresa.Oras = "New York";
+            elev.Adresa.Blocul = "121";
+            elev.Adresa.Apartamentul = 85;
+
+
+            Elev elev2 = new Elev
+            {
+                Nume = "Steel",
+                Prenume = "Anastasia",
+                IDNP = "2012012004458",
+                Clasa = 6,
+                DataNaserii = new DateTime(1997, 9, 15),
+                Genul = Gen.Feminin,
+                Adresa = new Address()
+            };
+
+            elev2.Adresa.Strada = "Broklyn";
+            elev2.Adresa.Oras = "New York";
+            elev2.Adresa.Blocul = "15";
+            elev2.Adresa.Apartamentul = 5;
+
+            Elev elev3 = new Elev();
+            elev3.Nume = "Steel43";
+            elev3.Prenume = "Anastasia43";
+            elev3.IDNP = "2012012004458";
+            elev3.Clasa = 6;
+            elev3.DataNaserii = new DateTime(1900, 9, 15);
+            elev3.Genul = Gen.Feminin;
+            elev3.Adresa = new Address();
+
+            elev3.Adresa.Strada = "Broklyn";
+            elev3.Adresa.Oras = "New York";
+            elev3.Adresa.Blocul = "15";
+            elev3.Adresa.Apartamentul = 5;
+
+            Elev elev4 = new Elev
+            {
+                Nume = "Steel",
+                Prenume = "Anastasia",
+                IDNP = "2012012004458",
+                Clasa = 6,
+                DataNaserii = new DateTime(1997, 1, 15),
+                Genul = Gen.Feminin,
+                Adresa = new Address()
+            };
+
+            Elev elev5 = new Elev
+            {
+                Nume = "Steel",
+                Prenume = "Anastasia",
+                IDNP = "2012012004458",
+                Clasa = 6,
+                DataNaserii = new DateTime(1987, 3, 25),
+                Genul = Gen.Feminin,
+                Adresa = new Address()
+            };
+
+            Elev elev6 = new Elev
+            {
+                Nume = "Steel",
+                Prenume = "Anastasia",
+                IDNP = "2012012004458",
+                Clasa = 6,
+                DataNaserii = new DateTime(2005, 6, 30),
+                Genul = Gen.Masculin,
+                Adresa = new Address()
+            };
+
+
+            var listaDeElevi = new List<Elev>();
+
+            listaDeElevi.Add(elev);
+            listaDeElevi.Add(elev3);
+            listaDeElevi.Add(elev2);
+            listaDeElevi.Add(elev4);
+            listaDeElevi.Add(elev5);
+            listaDeElevi.Add(elev6);
+
+            return listaDeElevi;
+        }
+
+    }
+}
--- student/Program.cs
+++ student/Program.cs
@@ -4,85 +4,119 @@ using System.Collections.Generic;
 namespace student
 {
     class Program
+
+
     {
-        static void Main(string[] args)
+
+        public static void Main(string[] args)
         {
-            Elev elev = new Elev();
-            elev.Nume = "Grey";
-            elev.Prenume = "Christian";
-            elev.IDNP = "2012012004459";
-            elev.Clasa = 8;
-            elev.DataNaserii = new DateTime(1998, 8, 11);
-            elev.Genul = Gen.Masculin;
-            elev.Adresa = new Address();
-
-            elev.Adresa.Strada = "Central Park";
-            elev.Adresa.Oras = "New York";
-            elev.Adresa.Blocul = "121";
-            elev.Adresa.Apartamentul = 85;
+            var dbCopii = new DbCopchii();
 
 
-            Elev elev2 = new Elev();
-            elev2.Nume = "Steel";
-            elev2.Prenume = "Anastasia";
-            elev2.IDNP = "2012012004458";
-            elev2.Clasa = 6;
-            elev2.DataNaserii = new DateTime(2000, 9, 15);
-            elev2.Genul = Gen.Feminin;
-            elev2.Adresa = new Address();
+            List<Elev> listaDeElevi = dbCopii.GetCophii();
 
-            elev2.Adresa.Strada = "Broklyn";
-            elev2.Adresa.Oras = "New York";
-            elev2.Adresa.Blocul = "15";
-            elev2.Adresa.Apartamentul = 5;
+            var eleviiDeGenulMasculin = new List<Elev>();
+            var eleviiDeGenulFeminin = new List<Elev>();
 
-            Elev elev3 = new Elev();
-            elev3.Nume = "Steel43";
-            elev3.Prenume = "Anastasia43";
-            elev3.IDNP = "2012012004458";
-            elev3.Clasa = 6;
-            elev3.DataNaserii = new DateTime(1900, 9, 15);
-            elev3.Genul = Gen.Feminin;
-            elev3.Adresa = new Address();
-                
-            elev3.Adresa.Strada = "Broklyn";
-            elev3.Adresa.Oras = "New York";
-            elev3.Adresa.Blocul = "15";
-            elev3.Adresa.Apartamentul = 5;
+            //if (new List<Elev>().Count == 0)
+            //{
+            //    Console.WriteLine("EMPTY LIST");
+            //    Console.ReadLine();
+            //    //Environment.Exit(0);
+            //};
 
-
-            var listaDeElevi = new List<Elev>();
-
-            listaDeElevi.Add(elev);
-            listaDeElevi.Add(elev2);
-            listaDeElevi.Add(elev3);
-
-            int result = DateTime.Compare(elev.DataNaserii, elev2.DataNaserii);
-            string relationship;
-
-            if (elev.DataNaserii < elev2.DataNaserii)
+            foreach (Elev elev in listaDeElevi)
             {
-                //elev mai in varsta ca elev 2
+                if (elev.Genul == Gen.Masculin)
+                {
+                    eleviiDeGenulMasculin.Add(elev);
+                }
+                else
+                {
+                    eleviiDeGenulFeminin.Add(elev);
+                }
             }
-            else if(elev.DataNaserii > elev2.DataNaserii)
-            {
-                //elev2 ii mai in varsta
-            }
-            else
-            {
-                //deo seama
-            }
-            
 
-            if (result < 0)
-                relationship = "is earlier than";
-            else if (result == 0)
-                relationship = "is as the same time as";
-            else
-                relationship = "is later than";
-            Console.WriteLine("{0} {1} {2}",elev.Nume, relationship, elev2.Nume);
+
+            foreach(Elev elevNume in eleviiDeGenulMasculin)
+            {
+                Console.WriteLine($"Elevii de genul masculin sunt: {elevNume.Nume}");
+            }
+
+
+            foreach (Elev elevNume in eleviiDeGenulFeminin)
+            {
+                Console.WriteLine($"Elevii de genul feminin sunt: {elevNume.Nume}");
+            }
+
+
+
+
+
+
+
+            //int result = DateTime.Compare(elev.DataNaserii, elev2.DataNaserii);
+            //string relationship;
+
+            //if (elev.DataNaserii < elev2.DataNaserii)
+            //{
+            //    //elev mai in varsta ca elev 2
+            //}
+            //else if(elev.DataNaserii > elev2.DataNaserii)
+            //{
+            //    //elev2 ii mai in varsta
+            //}
+            //else
+            //{
+            //    //deo seama
+            //}
+
+
+            //if (result < 0)
+            //    relationship = "is earlier than";
+            //else if (result == 0)
+            //    relationship = "is as the same time as";
+            //else
+            //    relationship = "is later than";
+            //Console.WriteLine("{0} {1} {2}",elev.Nume, relationship, elev2.Nume);
+            //Console.ReadLine();
+
+            var maxValue = (listaDeElevi[0]).DataNaserii;
+            var minValue = (listaDeElevi[0]).DataNaserii;
+
+            foreach (Elev e in listaDeElevi)
+            {
+                if (e.DataNaserii < maxValue)
+                {
+                    maxValue = e.DataNaserii;
+                }
+
+                else if (e.DataNaserii > minValue)
+                {
+                    minValue = e.DataNaserii;
+                }
+
+            }
+
+            Console.WriteLine($"Elevul cel mai in varsta este: {maxValue}");
+            Console.WriteLine($"Elevul cel mai mic este: {minValue}");
             Console.ReadLine();
 
+            var secondmaxValue = (listaDeElevi[0]).DataNaserii;
+
+
+            foreach (Elev e in listaDeElevi)
+            {
+                if (e.DataNaserii > maxValue && secondmaxValue > e.DataNaserii)
+                {
+                    secondmaxValue = e.DataNaserii;
+                }
+            }
+            Console.WriteLine($"Al doilea cel mai in varsta elev: {secondmaxValue}");
+            Console.ReadLine();
         }
+
+
+
     }
 }
